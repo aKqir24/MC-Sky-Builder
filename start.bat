c6ce58086510a486bdc02551f00789aa9444eacc
Akqir <142222025+aKqir24@users.noreply.github.com>
2024-03-07 02:30:51 +0800

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,7 +1,10 @@
 color f0
+@echo off
 cls
+
 echo You Might Need Some Internet To Install The Requirements
-s
+@echo off
+cls
 pip install keyboard
 cls
 pip install colorama
@@ -10,5 +13,28 @@ pip install pillow
 cls
 pip install numpy
 cls
+
+if  errorlevel 1 goto ERROR
+echo Requirements Installed
+cls
+echo Running Program...
+cls
+echo Running Program..
+cls
+echo Running Program.
+cls
+echo Running Program..
+cls
+echo Running Program...
+cls
+goto EOF 
+
+
+:ERROR
+
+echo Please Install Python Or Check Your Internet Connection
+cmd /k
+exit /b 1
+:EOF 
 
 python mcsky.py
