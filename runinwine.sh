c181c57dabe9798db20556ce658dade1a5b3dd69
Akqir <142222025+aKqir24@users.noreply.github.com>
2024-08-10 20:55:01 +0800

    Update runinwine.sh

--- runinwine.sh
+++ runinwine.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-"
-For Linux Enviroment You Need To Run It In Wine
-"
-wine python window.py
+#For Linux Enviroment You Need To Run It In Wine
+Error="Please install 'Wine' then install 'python' in wine."
+if dpkg -l | grep ^ii | grep -i wine ; then
+  clear
+  wine python window.py
+else
+  echo $Error
+fi
