512d948920b6a17c3f4e193211af154b24c01880
Akqir <142222025+aKqir24@users.noreply.github.com>
2024-08-10 22:12:04 +0800

    Update runinwine.sh

--- runinwine.sh
+++ runinwine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# For Linux Enviroments You Need To Run This In With Wine
+# For Linux Enviroments You Need To Run This In With A Python Environment and Wine 
 Wine_Error="Error: Please install 'Wine' then install 'python' in wine."
 Wine_Fix="You Can Refer To This Ducumentation For Assistance: https://www.winehq.org/pipermail/wine-devel/2002-January/003468.html"
 
