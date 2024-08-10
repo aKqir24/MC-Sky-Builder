c03779fc94e73428b14801d7fc322a654d14239a
Akqir <142222025+aKqir24@users.noreply.github.com>
2024-08-10 21:07:50 +0800

    Update runinwine.sh

--- runinwine.sh
+++ runinwine.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
 #For Linux Enviroment You Need To Run It In Wine
-Error="Please install 'Wine' then install 'python' in wine."
+Wine_Error="Please install 'Wine' then install 'python' in wine."
 if dpkg -l | grep ^ii | grep -i wine ; then
-  clear
-  wine python window.py
+  wine --verion
+  if wine python window.py ; then
+    echo "Program Launched!!"
+  else
+    echo "You forgot to install 'python' in wine!! " 
+  fi
 else
-  echo $Error
+  echo $Wine_Error
 fi
