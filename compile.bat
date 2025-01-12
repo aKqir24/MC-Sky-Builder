73c37e1978af737ca764f4da75abe214282c4b4d
aKqir24 <Akqir@protonmail.com>
2025-01-12 08:54:44 -0400

    MInor Update

--- compile.bat
+++ compile.bat
@@ -1,12 +1,7 @@
-@echo off
 set name=MC-Sky-Builder
 set icon=resource\icon.ico
 set module_path=libraries
 set work_folder=exe
 
-
-if Python --version if Python -m PyInstaller --version (
-    Python -m PyInstaller -w -i %icon% -D --optimize 2 --clean  --workpath %work_folder% --contents-directory %module_path% -n %name% window.py
-) else (
-    echo "Please update `python` then install PyInstaller and read the error logs!!"
-)
+rem Compile To Exe
+Python -m PyInstaller -w -i %icon% -D --optimize 2 --clean  --workpath %work_folder% --contents-directory %module_path% -n %name% window.py
